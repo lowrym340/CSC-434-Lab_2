15faa5da8cdec6ec50e9e4925c5bb316c488187e
lowrym340 <lowrym340@strose.edu>
2017-02-06 10:35:40 -0500

    Testing Eclipse Upload

--- SoftEngLab1/src/Hello.java
+++ SoftEngLab1/src/Hello.java
@@ -7,6 +7,14 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Matt Lowry");
+		System.out.println("Test");
+		System.out.println("Test2");
+
+		System.out.println("Hello World!");
+		
+		System.out.println("This is from my home computer.");
+
+//Hey!!
 	}
 
 }
